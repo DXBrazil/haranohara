24a7b45858f4dceb79a046a4092f9d6dffe8a854
Fabricio Catae <fcatae@microsoft.com>
2017-07-26 20:17:10 -0500

    add welcome picture

--- HaraNoHara/Controllers/MessagesController.cs
+++ HaraNoHara/Controllers/MessagesController.cs
@@ -2,8 +2,10 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
+using Microsoft.Bot;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using System;
 
 namespace HaraNoHara
 {
@@ -17,7 +19,7 @@ namespace HaraNoHara
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
             if (activity.Type == ActivityTypes.Message)
-            {
+            {               
                 await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
             }
             else
@@ -40,7 +42,20 @@ namespace HaraNoHara
                 // Handle conversation state changes, like members being added and removed
                 // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
                 // Not available in all channels
+                var replyMessage = message.CreateReply("Ola!");
+
+                replyMessage.Attachments.Add(new Attachment()
+                {
+                    ContentUrl = "https://pbs.twimg.com/profile_banners/22566954/1429286628/1500x500",
+                    ContentType = "image/png",
+                    Name = "haranohara.png"
+                });
+
+                var connector = new ConnectorClient(new Uri(message.ServiceUrl));
+
+                connector.Conversations.ReplyToActivity(replyMessage);
             }
+
             else if (message.Type == ActivityTypes.ContactRelationUpdate)
             {
                 // Handle add/remove from contact lists
